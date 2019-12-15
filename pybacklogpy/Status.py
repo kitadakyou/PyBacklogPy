2f319d9815b769f1e1a65e0dec4dafa012ae7cc4
Hikaru ETO <kitadakyou@yahoo.co.jp>
2019-12-15 21:45:58 +0900

    任意の状態作成機能に対応

--- pybacklogpy/Status.py
+++ pybacklogpy/Status.py
@@ -7,6 +7,93 @@ from pybacklogpy.modules import RequestSender
 
 class Status:
     def __init__(self, config: Optional[BacklogConfigure] = None):
-        self.base_path = 'statuses'
+        self.base_path = 'projects/'
         _config = config if config else None
         self.rs = RequestSender(_config)
+
+    def add_status(self,
+                   project_id_or_key: str,
+                   name: str,
+                   color: str,
+                   ) -> Response:
+        """
+        権限メソッドURLURL パラメーターリクエストパラメーターレスポンス例
+        https://developer.nulab.com/ja/docs/backlog/api/2/add-status/
+
+        :param project_id_or_key: プロジェクトのID または プロジェクトキー
+        :param name: 状態の名前
+        :param color: 状態の背景色；以下から指定”#ea2c00””#e87758””#e07b9a””#868cb7””#3b9dbd””#4caf93””#b0be3c””#eda62a””#f42858””#393939”
+
+        :return: レスポンス
+        """
+
+        path = self.base_path + project_id_or_key + '/statuses'
+        payloads = {'name': name, 'color': color}
+        return self.rs.send_post_request(path=path, request_param=payloads)
+
+    def update_status(self,
+                      project_id_or_key: str = None,
+                      status_id: int = None,
+                      name: str = None,
+                      color: str = None,
+                      ) -> Response:
+        """
+        権限メソッドURLURL パラメーターリクエストパラメーターレスポンス例
+        https://developer.nulab.com/ja/docs/backlog/api/2/update-status/
+
+        :param project_id_or_key: プロジェクトのID または プロジェクトキー
+        :param status_id: 状態のID
+        :param name: 状態の名前
+        :param color: 状態の背景色；以下から指定”#ea2c00””#e87758””#e07b9a””#868cb7””#3b9dbd””#4caf93””#b0be3c””#eda62a””#f42858””#393939”
+
+        :return: レスポンス
+        """
+
+        path = self.base_path + '{project_id_or_key}/statuses/{status_id}'\
+            .format(project_id_or_key=project_id_or_key, status_id=status_id)
+        payloads = {}
+        if name:
+            payloads['name'] = name
+        if color:
+            payloads['color'] = color
+        return self.rs.send_patch_request(path=path, request_param=payloads)
+
+    def delete_status(self,
+                      project_id_or_key: str,
+                      status_id: int,
+                      substitute_status_id: int,
+                      ) -> Response:
+        """
+        権限メソッドURLURL パラメーターリクエストパラメーターレスポンス例
+        https://developer.nulab.com/ja/docs/backlog/api/2/delete-status/
+
+        :param project_id_or_key: プロジェクトのID または プロジェクトキー
+        :param status_id: 状態のID
+        :param substitute_status_id: 紐づく課題を付け替える先の状態のID。削除対象の状態が設定されている課題がある場合、このパラメーターで指定した状態へ一括変更します。
+
+        :return: レスポンス
+        """
+
+        path = self.base_path + '{project_id_or_key}/statuses/{status_id}' \
+            .format(project_id_or_key=project_id_or_key, status_id=status_id)
+        payloads = {'substituteStatusId': substitute_status_id}
+        return self.rs.send_delete_request(path=path, request_param=payloads)
+
+    def update_order_of_status(self,
+                               project_id_or_key: str,
+                               status_id: List[int],
+                               ) -> Response:
+        """
+        権限メソッドURLURL パラメーターリクエストパラメーターレスポンス例
+        https://developer.nulab.com/ja/docs/backlog/api/2/update-order-of-status/
+
+        :param project_id_or_key: プロジェクトのID または プロジェクトキー
+        :param status_id: 表示順に並べた、状態のIDのリスト。そのプロジェクトで使える全ての状態を渡してください。表示順には以下の制限があります未対応は先頭にあること完了は末尾にあること処理中は処理済みよりも前にあること
+
+        :return: レスポンス
+        """
+
+        path = self.base_path + '{project_id_or_key}/statuses/updateDisplayOrder'\
+            .format(project_id_or_key=project_id_or_key)
+        payloads = {'statusId[]': status_id}
+        return self.rs.send_patch_request(path=path, request_param=payloads)
--- tests/test_status.py
+++ tests/test_status.py
@@ -1,20 +1,50 @@
 import unittest
 
 from pybacklogpy.Status import Status
-from tests.utils import response_to_json
+from tests.utils import get_project_id_and_key, response_to_json
 
 
 class TestStatus(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # プロジェクト ID を取得、存在しなければ作成する
+        cls.project_id, cls.project_key = get_project_id_and_key()
         cls.status = Status()
 
-    def test_get_status_list(self):
-        response = self.status.get_status_list()
-        self.assertTrue(response.ok, msg='状態一覧の取得に失敗')
-        response_list = response_to_json(response)
-        self.assertIsInstance(response_list, list, msg='状態一覧の取得に失敗')
+    def test_status(self):
+        from pybacklogpy.Licence import Licence
+        l = Licence()
+        if response_to_json(l.get_licence())['licenceTypeId'] == 11:
+            self.skipTest(reason='無料版ライセンスだとカスタム状態の設定ができない')
+
+        response_add_status = self.status.add_status(
+            project_id_or_key=self.project_key,
+            name='保留',
+            color='#ea2c00',
+        )
+        self.assertTrue(response_add_status.ok, msg='状態の追加に失敗')
+        status_id = response_to_json(response_add_status)['id']
+
+        response_update_status = self.status.update_status(
+            project_id_or_key=self.project_key,
+            status_id=status_id,
+            name='明日やる',
+            color='#868cb7',
+        )
+        self.assertTrue(response_update_status.ok, msg='状態の更新に失敗')
+
+        response_update_order = self.status.update_order_of_status(
+            project_id_or_key=self.project_key,
+            status_id=[1, 2, status_id, 3, 4],
+        )
+        self.assertTrue(response_update_order.ok, msg='状態の順序の更新に失敗')
+
+        response_delete_status = self.status.delete_status(
+            project_id_or_key=self.project_key,
+            status_id=status_id,
+            substitute_status_id=1,
+        )
+        self.assertTrue(response_delete_status.ok, msg='状態の削除に失敗')
 
 
 if __name__ == '__main__':
