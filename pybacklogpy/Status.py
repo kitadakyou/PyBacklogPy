30d1c08c388a970abd3a485a333e6d69a34df45d
Hikaru ETO <kitadakyou@yahoo.co.jp>
2019-12-15 20:59:40 +0900

    非推奨のAPIを削除

--- pybacklogpy/Status.py
+++ pybacklogpy/Status.py
@@ -1,5 +1,5 @@
 from requests import Response
-from typing import Optional
+from typing import List, Optional
 
 from pybacklogpy.BacklogConfigure import BacklogConfigure
 from pybacklogpy.modules import RequestSender
@@ -10,14 +10,3 @@ class Status:
         self.base_path = 'statuses'
         _config = config if config else None
         self.rs = RequestSender(_config)
-
-    def get_status_list(self) -> Response:
-        """
-        状態一覧の取得
-        https://developer.nulab.com/ja/docs/backlog/api/2/get-status-list/
-
-        :return: レスポンス
-        """
-
-        path = self.base_path
-        return self.rs.send_get_request(path=path, url_param={})
